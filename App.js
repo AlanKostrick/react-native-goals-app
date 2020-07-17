75063295dd7ee6ee66f562e2d5423785cd2a6e66
Alan Kostrick <alan.kostrick@civotek.com>
2020-07-17 15:21:50 -0500

    add stylesheet object

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <View>
-
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder="Course Goal"
+                    style={styles.input}
+                />
+                <Button title="ADD" />
             </View>
             <View>
 
@@ -16,5 +19,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        borderColor: 'black',
+        borderWidth: 1, padding: 10,
+        width: '80%'
+    }
 });
